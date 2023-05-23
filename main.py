c5f2ef1c30c9e2ce35f65b05256febd62286c322
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-05-23 21:24:43 +0700

    belajar variable

---
+++ main.py
@@ -0,0 +1,29 @@
+# Variable adalah tempat menyimpan data 
+
+#Menaruh/ assignments nilai
+
+a = 100
+b = 11
+c = 13
+
+# Pemanggilan pertama
+print("Ini Pemanggilan Pertama")
+print(a + b + c + c + c + c + c + c + c )
+print("Nilai ujian kamu", a, ",congratulations")
+
+# Penamanaan 
+nilai_x = 100 #menggunakan underscore
+juta10 = 100 #gaboleh ada angka di depan 
+
+#Pemanggilan Kedua 
+print("")
+print("Ini Pemanggilan Kedua")
+print(juta10 + nilai_x)
+
+a = 50
+print("Nilai a =", a)
+
+#assignment indirect
+b = a 
+print(b)
+
