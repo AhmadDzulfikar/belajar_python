8e1bd58ad9403209ea95e814b13061edc467317b
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-05-27 15:20:11 +0700

    Ambil data user

---
+++ ambildata/coba.py
@@ -0,0 +1,13 @@
+#form input
+nama = input("Masukkan nama anda = ")
+tgl_lahir = int(input("Masukkan tanggal lahir anda = "))
+bln_lahir = input("Masukkan bulan lahir anda = ")
+thn_lahir = int(input("Masukkan tahun lahir anda = "))
+
+#hasil
+print("")
+print("\*BIODATA ANDA*/")
+print("Nama =", nama)
+print("Tanggal Lahir =", tgl_lahir)
+print("Bulan lahir =", bln_lahir)
+print("Tahun lahir =", thn_lahir)
---
+++ ambildata/main.py
@@ -0,0 +1,21 @@
+#Mengambil data user 
+
+#data yang diinput pasti string
+data = input("Masukkan Nama:")
+
+print("Nama:", data, "type =", type(data))
+
+#cara input data int
+numbers = int(input("Masukkan angka:"))
+
+print("Nomor =", numbers, "type:", type(numbers))
+
+#cara input data float
+numbers = float(input("Masukkan angka:"))
+
+print("Nomor =", numbers, "type:", type(numbers))
+
+#cara input nilai boolean
+biner = bool(int(input("Masukkan nilai biner:"))) #dikasih tipedata int biar angka 0 bisa menjadi false
+
+print("Nilai =", biner, "bertipe =", type(biner))
