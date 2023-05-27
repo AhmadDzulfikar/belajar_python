5b87e9b31e8b7f16ec32ecebff325b8b9c43ade8
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-05-27 22:32:15 +0700

    operasi artimatika 1

---
+++ aritmatika_8/main.py
@@ -0,0 +1,32 @@
+# operasi aritmatika
+
+a = 11
+b = 5
+
+# Operasi tambah +
+hasil = a + b 
+print(a, "+", b, "=", hasil)
+
+# Operasi pengurangan -
+hasil = a - b 
+print(a, "-", b, "=", hasil)
+
+# Operasi perkalian *
+hasil = a * b 
+print(a, "*", b, "=", hasil)
+
+# Operasi perkalian /
+hasil = a / b 
+print(a, "/", b, "=", hasil)
+
+# Operasi eksponen (pangkat) **
+hasil = a ** b
+print(a, "pangkat", b, "=", hasil)
+
+# Operasi modulus (sisa pembagian) %
+hasil = a % b
+print(a, "modulus", b, "=", hasil)
+
+# Operasi floor devision //
+hasil = a // b
+print(a, "//", b, "=", hasil)
