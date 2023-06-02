44d7c96349e7871eb76bbad01f763fd9a70cd380
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-02 23:13:27 +0700

    test

--- perhitungan_9/lat.py
+++ perhitungan_9/lat.py
@@ -6,7 +6,7 @@ kelvin = 5/9 * (fahrenheit - 32) + 273
 print("suhu", fahrenheit, "diubah ke kelvin adalah", kelvin, "kelvin")
 
 
-kelvin = float(input("masukan angka kelvin disini = "))
+kelvin = float(input("masunkan angka kelvin disini = "))
 
 fahrenheit = (9/5) * (kelvin - 273) + 32
 print("suhu adamlah", fahrenheit)
