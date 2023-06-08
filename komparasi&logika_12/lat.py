b758dc64ca5c8bb64d2621871ef3f67bed6144a8
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-08 10:09:07 +0700

    lat komparasi dan logika ke-2

--- komparasi&logika_12/lat.py
+++ komparasi&logika_12/lat.py
@@ -20,3 +20,23 @@ print("\n lebih dari 8 dan kurang dari 11 =", antara8dan11)
 
 hasil = antara0dan5 or antara8dan11
 print("angka yang anda masukkan =", hasil)
+
+print("\n", 10*"=", "\n")
+''' KERJAKAN 
+        2.)'''
+
+inputUser = float(input("Masukkan angka yang bernilai \n antara 5 dan 8 \n atau \n lebih dari 11 \n :"))
+
+# Antara 5 dan 8
+kurangDari_0 = inputUser < 0 
+print("\n kurang dari 0 =", kurangDari_0)
+lebihDari_5 = inputUser > 5
+kurangDari_8 = inputUser < 8
+antara5dan8 = lebihDari_5 and kurangDari_8
+print("\n lebih dari 5 kurang dari 8 =", antara5dan8)
+# Lebih dari 11
+lebihDari_11 = inputUser > 11
+print("\n lebih dari 11 =", lebihDari_11)
+
+hasil = kurangDari_0 or antara5dan8 or lebihDari_11
+print("angka yang anda masukkan =", hasil)
