b84669a518b269dc4e6046c37e4262b669e80640
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-09 21:47:53 +0700

    operasi bit OR

---
+++ operasi_bitwise/main.py
@@ -0,0 +1,15 @@
+# Operator bitwise, operasi biner, binary
+
+# OR (|), AND (&), XOR (^), NOT(~)
+
+a = 5
+b = 7
+
+# Bitwise OR (|)
+c = a | b 
+print("\n ======OR======")
+print('nilai :', a, 'binary :', format(a,'08b'))
+print('nilai :', b, 'binary :', format(b, '08b'))
+print('-------------------------------(|)')
+
+
