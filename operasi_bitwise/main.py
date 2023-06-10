9697d3053eb5c8c654ae64eaa7dc5d246297a1a2
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-10 12:01:11 +0700

    operasi bitwise AND

--- operasi_bitwise/main.py
+++ operasi_bitwise/main.py
@@ -8,8 +8,15 @@ b = 7
 # Bitwise OR (|)
 c = a | b 
 print("\n ======OR======")
-print('nilai :', a, 'binary :', format(a,'08b'))
+print('nilai :', a, 'binary :', format(a, '08b'))
 print('nilai :', b, 'binary :', format(b, '08b'))
 print('-------------------------------(|)')
+print('nilai :', c, 'binary :', format(c, '08b'))
 
+# Bitwise AND (&)
+c = a & b 
+print('\n ======AND======')
+print("nilai :", a, 'binary :', format(a, '08b'))
+print("nilai :", b, 'binary :', format(b, '08b'))
+print("nilai :", c, 'binary :', format(c, '08b'))
 
