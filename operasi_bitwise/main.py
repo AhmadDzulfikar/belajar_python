6abbc0169d286443b9e6ad53a9e6960338ceccf5
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-10 15:24:41 +0700

    operasi bitwise

--- operasi_bitwise/main.py
+++ operasi_bitwise/main.py
@@ -20,3 +20,38 @@ print("nilai :", a, 'binary :', format(a, '08b'))
 print("nilai :", b, 'binary :', format(b, '08b'))
 print("nilai :", c, 'binary :', format(c, '08b'))
 
+# Bitwise XOR (^)
+c = a ^ b 
+print('\n ======XOR======')
+print("nilai :", a, 'binary :', format(a, '08b'))
+print("nilai :", b, 'binary :', format(b, '08b'))
+print("nilai :", c, 'binary :', format(c, '08b'))
+
+# Bitwise NOT (~)
+c = ~a 
+print('\n ======NOT======')
+print("nilai :", a, 'binary :', format(a, '08b'))
+print('----------------------------(~)')
+print("nilai :", c, 'binary :', format(c, '08b'))
+print('----------------------------(^)')
+d = 0b00000110
+e = 0b11111111
+print('nilai :', e^d, 'binary :', format(e^d, '08b'))
+
+# Shifting 
+
+# Shift Right (>>)
+c = a >> 2
+print('\n ======>>======')
+print("nilai :", a, 'binary :', format(a, '08b'))
+print('----------------------------(>>)')
+print("nilai :", c, 'binary :', format(c, '08b'))
+
+# Shift Left (<<)
+c = a << 2
+print('\n ======<<======')
+print("nilai :", a, 'binary :', format(a, '08b'))
+print('----------------------------(<<)')
+print("nilai :", c, 'binary :', format(c, '08b'))
+
+
