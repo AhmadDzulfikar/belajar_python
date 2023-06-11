276e761a98ace621222649550e50eb17410a86bf
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-11 17:01:41 +0700

    operator assignment aritmatika

---
+++ operator_assignment/main.py
@@ -0,0 +1,28 @@
+# Operasi yang dapat dilakukan dengan penyingkatan
+
+# Operasi ditambah denga assignment
+
+a = 5 # adalah assignment
+print("nilai a:", a)
+
+a += 1 # artinya adalah nilai a + 1
+print('nilai a += 1, nilai a menjadi:', a)
+
+a -= 2 # artinya adalah nilai a - 2
+print('nilai a -= 2, nilai a menjadi:', a)
+
+a *= 2 # artinya adalah nilai a * 2
+print('nilai a *= 2, nilai a menjadi:', a)
+
+a /= 2 # artinya adalah nilai a / 2
+print('nilai a /= 2, nilai a menjadi:', a)
+
+b = 10
+
+b %= 3 # artinya adalah nilai b % 3 (Modulus)
+print('nilai b %= 3, nilai b adalah:', b)
+
+b = 10
+
+b //= 2 # artinya adalah nilai b // 2 (floor devision)
+print('nilai b //= 2, nilai b adalah:', b)
