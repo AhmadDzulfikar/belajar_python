8b68ae815a3cb474e303ef4b2d555e987094dfcb
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-07-28 07:30:45 +0700

    while loop

---
+++ WHILE_loop_25/main.py
@@ -0,0 +1,16 @@
+# while loop 
+
+# while kondisi:
+#     aksi Inii
+#     aksi ituu
+
+# akhir dari program
+
+angka = 1
+print(f'angka sekarang -> {angka}')
+
+while angka < 5:
+    angka += 1
+    print(f'angka sekarang -> {angka}')
+    print('UNLIMITED?')
+print('mang eak?')
