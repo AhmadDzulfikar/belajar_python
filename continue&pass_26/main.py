b134e5d094c4935be56697e1fa7e62aa771bd961
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-07-31 17:46:14 +0700

    Continue & pass 2

--- continue&pass_26/main.py
+++ continue&pass_26/main.py
@@ -2,14 +2,29 @@
 
 # pass -> dia berfungsi sebagai dummy 
 
-angka = 0 
+# angka = 0 
+
+# while angka < 5:
+#     angka += 1
+#     if angka == 3:
+#         # print('OOMMAAGAA')
+#         pass # ini tidak akan dieksekusi
+
+#     print(angka)  
+
+# continue 
+angka = 0
+
+print(f"angka sekarang -> {angka}")
 
 while angka < 5:
     angka += 1
+    print(f"angka sekarang -> {angka}") # aksi 1
+
     if angka == 3:
-        # print('OOMMAAGAA')
-        pass # ini tidak akan dieksekusi
+        print('sudah 3 nichh') 
+        continue # continue akan membuat loop meloncat kembali ke awal
 
-    print(angka)  
+    print("mantapppp") # aksi 2
 
-# continue 
+print("berhenti")
